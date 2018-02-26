42df86b7b7235abd98e18bcdab2c7c3a88ff6f50
dadapawa <36859672+dadapawa@users.noreply.github.com>
2018-02-26 13:47:15 -0500

    Ajout

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
-        <p> TEST </p>
+        <p> TEST AJOUT</p>
 
         <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
         <script>

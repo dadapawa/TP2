d328a49fd428685d1771e0e6c8dd4195710f4679
dadapawa <36859672+dadapawa@users.noreply.github.com>
2018-02-27 18:03:51 -0500

    Header.php update (full)
    Commented section is still under testing (for Active button)

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,48 @@
-<nav>
-	Pages :
-	<ul>
-		<li><a href="?page=accueil">Accueil</a></li>
-        <li><a href="?page=inscription">Inscription</a></li>
-        <li><a href="?page=presentation">Presentation</a></li>
-	</ul>
-</nav>
+
+<div class="TitleRow">
+	<img style="height:90px; width:90px; margin:5px;" src="img/LeagueOfLegends.png" alt="FortniteBR TEMP" >
+	<h1 class="Title";>Team Of Legends</h1>
+	<nav>
+			<ul class="Menu">
+			<?php /* if(isset($_GET['page'])) : ?>
+				<?php if($_GET['page'] == 'accueil'): ?>
+					<li><a class="active" href="?page=accueil">Accueil</a></li>
+				<?php else : ?>
+					<li><a href="?page=accueil">Accueil</a></li>
+				<?php endif; ?>
+
+				<?php if($_GET['page'] == 'inscription') : ?>
+					<li><a class="active" href="?page=inscription">Inscription</a></li>
+				<?php else : ?>
+					<li><a href="?page=inscription">Inscription</a></li>
+				<?php endif; ?>
+
+				<?php if($_GET['page'] == 'presentation') : ?>
+					<li><a class="active" href="?page=presentation">Presentation</a></li>
+				<?php else : ?>
+					<li><a href="?page=presentation">Presentation</a></li>
+				<?php endif; ?>
+			<?php endif; */ ?>
+					<li><a class="active" href="?page=accueil">Accueil</a></li>
+					<li><a href="?page=inscription">Inscription</a></li>
+					<li><a href="?page=presentation">Presentation</a></li>
+				<form>
+					<input type="text" placeholder="Search..">
+					<button type="submit">Envoyer</button>
+				</form> 
+
+			</ul>
+	</nav>
+
+</div >
+<div class="Banner" >
+	<img class="BannerImage" src="img/Banner1.jpg" alt="Banner 1 of LoL" >
+	<img class="BannerImage" src="img/Banner2.jpg" alt="Banner 2 of LoL" >
+	<img class="BannerImage" src="img/Banner3.jpg" alt="Banner 3 of LoL" >
+	<img class="BannerImage" src="img/Banner4.jpg" alt="Banner 4 of LoL" >
+	<img class="BannerImage" src="img/Banner5.jpg" alt="Banner 5 of LoL" >
+	<img class="BannerImage" src="img/Banner6.jpg" alt="Banner 6 of LoL" >
+	<img class="BannerImage" src="img/Banner7.jpg" alt="Banner 7 of LoL" >
+	<img class="BannerImage" src="img/Banner8.jpg" alt="Banner 8 of LoL" >
+
+</div>

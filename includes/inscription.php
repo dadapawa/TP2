e91bbd460266405b77be52cd03059dc2f6864319
dadapawa <36859672+dadapawa@users.noreply.github.com>
2018-02-27 18:04:58 -0500

    Inscription.php full code
    Pas mal complet. Manquera qu'a faire le Java/PHP verification.

--- includes/inscription.php
+++ includes/inscription.php
@@ -1,1 +1,36 @@
-<p> INSCRIPTION </p>
+<h2 style="margin-left:5%;"> INSCRIPTION </h2>
+
+<p> Voici formulaire pour joindre. soumission accepter seulement avec don de 100.000$ </p>
+
+<form class="Inscription">
+    <div class="Sujet">
+        <a>Nom</a> <br>
+        <a>Courriel</a> <br>
+        <a>Jeux</a>
+    </div>
+    <div class="Boite">
+        <input type="text" name="nom">
+        <input type="text" name="jeux">
+        <input type="text" name="courriel">
+    </div>
+    <div class="Sujet">
+        <a>Prénom</a> <br>
+        <a>Discord</a> <br>
+        <a>Disponibilités</a>
+    </div>
+    <div class="Boite">
+        <input type="text" name="prenom">
+        <input type="text" name="discord">
+        <input type="text" name="dispo" placeholder="Matin, Soir, 16h à 22h...">
+    </div>
+        <a style="margin-left:5%;">Pourquoi Désirez-vous rejoindre notre équipe? </a> 
+        <textarea rows="6" style="width:89%; margin-left:5%; padding:7px;" type="text" name="Raisons"></textarea>
+    <div class="Sujet">
+        Date de début: 
+    </div>
+    <div class="Boite">
+        <input style="width:35%;"type="date" name="dateDebut">
+    </div>
+    
+    <button type="submit">Envoyer</button>
+</form>

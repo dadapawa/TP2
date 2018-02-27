872a243643a4074eed7e5b27e4b3d526b7f6145a
Jimmy Robitaille <jimmy-robitaille@hotmail.com>
2018-02-26 20:16:11 -0500

    news.php + css terminé

--- includes/news.php
+++ includes/news.php
@@ -1,1 +1,20 @@
-<p> NEWS </p>
+<h4> Nouvelles </h4>
+<div class="news_1"> 
+    <img class="img_news" src="img/news_img1.jpg" alt="Image_News_1" />
+    <p class="news_texte"><span><a href="https://euw.leagueoflegends.com/fr/news/store/sales/les-figurines-mini-ressortent-du-coffre">
+    Les figurines Mini ressortent du coffre!</a></span>
+    La première série des figurines Gardiens des étoiles en édition Mini...</P>
+
+</div>
+<div class="news_2"> 
+    <img class="img_news" src="img/news_img2.jpg" alt="Image_News_2" />
+    <p class="news_texte"><span><a href="https://euw.leagueoflegends.com/fr/news/store/sales/emotes-moitie-prix-debut-de-la-premiere-partie">
+    Emotes à moitié prix: début de la première partie</a></span>
+    Obtenez les emotes Chaton triste, Comment oses-tu et Jolification...</p>
+</div>
+<div class="news_3"> 
+    <img class="img_news" src="img/news_img3.jpg" alt="Image_News_3" />
+    <p class="news_texte"><span><a href="https://euw.leagueoflegends.com/fr/news/store/sales/promos-anticipees-de-fevrier-26/02/18-06/03/18">
+    Promos anticipées de février</a></span>
+    Gardiens des étoiles, Boss de combat et bien d'autres sont dans les...</p>
+</div>
